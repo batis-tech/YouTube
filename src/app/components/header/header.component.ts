9847388814b2e6a4847208907a1780729d0d6f9f
hasan <hasanbatis440@gmail.com>
2021-11-08 18:22:30 +0300

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit ,Inject,  Renderer2} from '@angular/core';
+
+import { DOCUMENT } from '@angular/common'
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  theme: Theme = 'dark-theme'
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2) {}
+
+  ngOnInit(){
+    this.initializeTheme();
+  }
+  switchTheme(){
+    this.document.body.classList.replace(this.theme, this.theme ==='light-theme'? (this.theme = 'dark-theme') : (this.theme = 'light-theme'))
+  }
+  initializeTheme = (): void =>
+   this.renderer.addClass(this.document.body, this.theme)
+}
+ export type Theme = 'light-theme' | 'dark-theme';
